cbdf5de56b9640b7bfcd8fbae55e65b634b27c10
fxue <xuefengwork@qq.com>
2016-07-15 17:14:29 +0800

    add comment

--- src/com/motoste/access/ChocolateChip.java
+++ src/com/motoste/access/ChocolateChip.java
@@ -2,17 +2,19 @@ package com.motoste.access;
 
 import com.motoste.access.dessert.Cookie;
 
+//This is a subclass
 public class ChocolateChip extends Cookie {
 	public ChocolateChip() {
 		System.out.println("ChocolateChip constructor");
 	}
-	
+
 	public void chomp() {
 		bite();
 	}
-	
+
 	public static void main(String[] args) {
 		ChocolateChip x = new ChocolateChip();
 		x.chomp();
 	}
+
 }
